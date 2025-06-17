94d7c4072fc89583a6f4cd42e07a91f3835f55fa
atu1ya <24225113@student.uwa.edu.au>
2025-06-17 21:48:50 +0800

    added git ignore and implemented correlation based pair selection and mean reversion

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
-
 import numpy as np
-
-##### TODO #########################################
-### IMPLEMENT 'getMyPosition' FUNCTION #############
-### TO RUN, RUN 'eval.py' ##########################
-
-nInst = 50
-currentPos = np.zeros(nInst)
-
+import itertools
 
 def getMyPosition(prcSoFar):
-    global currentPos
-    (nins, nt) = prcSoFar.shape
-    if (nt < 2):
-        return np.zeros(nins)
-    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
-    lNorm = np.sqrt(lastRet.dot(lastRet))
-    lastRet /= lNorm
-    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
-    currentPos = np.array([int(x) for x in currentPos+rpos])
-    return currentPos
+    n, t = prcSoFar.shape
+    if t < 21:
+        return np.zeros(n)
+
+    lookback = 20
+    returns = np.log(prcSoFar[:, -lookback:] / prcSoFar[:, -lookback-1:-1])
+
+    # Compute correlation matrix
+    corr = np.corrcoef(returns)
+
+    # Find best correlated pair
+    best_pair = None
+    best_corr = 0
+    for i, j in itertools.combinations(range(n), 2):
+        if corr[i, j] > best_corr and i != j:
+            best_corr = corr[i, j]
+            best_pair = (i, j)
+
+    if not best_pair:
+        return np.zeros(n)
+
+    i, j = best_pair
+    spread = prcSoFar[i, -1] - prcSoFar[j, -1]
+    spread_hist = prcSoFar[i, -lookback:] - prcSoFar[j, -lookback:]
+    spread_mean = spread_hist.mean()
+    spread_std = spread_hist.std()
+    z = (spread - spread_mean) / (spread_std + 1e-6)
+
+    # Mean reversion: if spread is wide, short the wider leg, long the narrower
+    pos = np.zeros(n)
+    if z > 1:
+        pos[i] = -1000
+        pos[j] = 1000
+    elif z < -1:
+        pos[i] = 1000
+        pos[j] = -1000
+
+    return pos
